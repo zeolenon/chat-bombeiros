dd75a3814e3354bebdc2e4273b8c9b10f9f50c3d
leo zen <leo.tassi@gmail.com>
2025-07-10 16:04:10 -0300

    atualizacao next e eslint

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['pdf-parse', 'pg'],
     // Otimizações para arquivos grandes
     optimizePackageImports: ['pdf-parse'],
   },
